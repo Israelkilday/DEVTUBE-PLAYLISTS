0435e7db8c2e81039f8c35709e85c70ee21c2fd4
Israel Kilday <israel.kilday@yahoo.com.br>
2024-07-16 22:48:36 -0300

    feat: add db.json

--- src/lib/axiox.ts
+++ src/lib/axiox.ts
@@ -1,6 +1,7 @@
+// baseURL: "https://api.jsonbin.io/v3/b/6696fc3ae41b4d34e412eaca",
 import axios from "axios";
-// baseURL: "https://video-player-mu-murex.vercel.app/",
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/"
+  baseURL: "https://my-json-server.typicode.com/Israelkilday/VIDEO-PLAYER",
 });
