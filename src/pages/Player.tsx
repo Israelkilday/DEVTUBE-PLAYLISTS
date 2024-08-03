1d579884f90a70ffe5862be7505568865de4a4c9
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-03 15:18:20 -0300

    feat: update margin in button

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -43,7 +43,7 @@ export function Player() {
             Deixar Feedback
           </button> */}
 
-          <button className="inline-flex animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-base font-medium text-neutral-400 transition-colors">
+          <button className="mb-2 inline-flex animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-base font-medium text-neutral-400 transition-colors">
             <a
               href="https://www.linkedin.com/in/israeldevfrontend/"
               download
