033f91188a32af56054e2cafc7c407544c15c739
Israel Kilday <israel.kilday@yahoo.com.br>
2024-08-06 23:48:13 -0300

    feat: add  module node.js and git

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -23,17 +23,17 @@ export function Lesson({
       onClick={handleclick}
       data-active={isCurrent}
       disabled={isCurrent}
-      className="flex items-center gap-3 text-base text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
+      className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
     >
       {isCurrent ? (
-        <PlayCircle className="h-4 w-4 text-emerald-400 lg:h-5 lg:w-5" />
+        <PlayCircle className="h-4 w-4 text-emerald-400" />
       ) : (
-        <Video className="h-4 w-4 text-zinc-500 lg:h-5 lg:w-5" />
+        <Video className="h-4 w-4 text-zinc-500" />
       )}
 
       <span>{title}</span>
 
-      <span className="ml-auto font-mono text-base text-zinc-500">
+      <span className="ml-auto font-mono text-sm text-zinc-500">
         {duration}
       </span>
     </button>
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,4 +1,3 @@
-// import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
@@ -30,20 +29,15 @@ export function Player() {
     <div className="min-h-screen items-center justify-center bg-zinc-950 text-zinc-50 lg:flex">
       <div className="flex flex-col bg-zinc-950 lg:w-[1100px]">
         <div className="px-5">
-          <h1 className="Text Shine boder-[1px] inline-flex w-full animate-shine border-b border-zinc-500 bg-[linear-gradient(110deg,#939393,45%,#1e2631,55%,#939393)] bg-[length:200%_100%] bg-clip-text pb-4 pt-6 font-montserrat text-4xl font-medium text-transparent lg:pt-7 lg:text-5xl">
+          <h1 className="Text Shine boder-[1px] inline-flex w-full animate-shine border-b border-zinc-500 bg-[linear-gradient(110deg,#939393,45%,#1e2631,55%,#939393)] bg-[length:200%_100%] bg-clip-text pb-4 pt-6 font-montserrat text-4xl font-medium text-transparent lg:pt-8 lg:text-5xl">
             DevTube Playlists
           </h1>
         </div>
 
-        <div className="items-center justify-between p-5 lg:flex lg:pb-0">
+        <div className="items-center justify-between p-5 lg:flex lg:pb-2">
           <Header />
 
-          {/* <button className="mt-5 flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white duration-200 hover:bg-violet-600">
-            <MessageCircle className="h-4 w-4" />
-            Deixar Feedback
-          </button> */}
-
-          <button className="mb-2 inline-flex animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-base font-medium text-neutral-400 transition-colors">
+          <button className="mb-2 mt-2 inline-flex animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-base font-medium text-neutral-400 transition-colors">
             <a
               href="https://www.linkedin.com/in/israeldevfrontend/"
               download
