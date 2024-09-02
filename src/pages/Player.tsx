039112538c4a6c3f0005f2312cd0e137033e929c
Israel Kilday <israel.kilday@yahoo.com.br>
2024-09-02 19:58:06 -0300

    refactor update link in button

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -39,7 +39,7 @@ export function Player() {
 
           <button className="mb-2 mt-2 inline-flex animate-shine items-center justify-center rounded-xl border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-4 py-2 text-base font-medium text-neutral-400 transition-colors">
             <a
-              href="https://www.linkedin.com/in/israeldevfrontend/"
+              href="https://www.linkedin.com/feed/update/urn:li:activity:7236489993605529600/"
               download
               target="blank"
               rel="noopener noreferrer"
